d3e464f971f75c199de81a0c645bd7ab23d33149
hiteshkumar8642 <hiteshkumar8642@gmail.com>
2024-03-01 00:32:13 +0530

    Database created

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import CollegeLoginView
+from . import views
 urlpatterns = [
+    path('',views.home,name='home'),
     path('', CollegeLoginView.as_view(), name='college_login'),
 ]
