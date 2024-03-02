98d7bce34b6c26fd15df3406219a0ca113b6efca
hiteshkumar8642 <hiteshkumar8642@gmail.com>
2024-03-02 16:17:11 +0530

    updated urls

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from userDetails.views import login
 from . import views
 urlpatterns = [
     path('',views.home,name='home'),
+    path('user/login',login,name='login'),
 ]
