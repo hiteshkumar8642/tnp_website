28d0c49ce558dcaeb7c6f0fa9cc3c7ef2d8757f9
Ritik Bansal <bansalritik108@gmail.com>
2024-04-20 00:06:20 +0530

    Added User profile page

--- userDetails/urls.py
+++ userDetails/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = [
     path("register" , views.register , name = "register"),
     path("login" , views.login , name = "login"),
+    path("SaveDetails",views.SaveDetails,name = "SaveDetails"),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path("userProfile" , views.userProfile , name="userProfile"),
 ]
