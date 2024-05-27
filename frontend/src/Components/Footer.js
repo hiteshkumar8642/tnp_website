b88dd557d4cfee452a0414d44578ee4253380c3c
Hitesh Kumar <hiteshkumar8642@gmail.com>
2024-05-27 22:12:11 +0530

    footer added

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,101 +1,121 @@
-import "../Styles/Footer.css";
+import React from 'react';
+import '../Styles/Footer.css';
 
-export default function Footer() {
-  const PageName = "Placement";
-  return (
-    <footer className="footer">
-      <div className="footer-top">
-        <div className="footer-section">
-          <h3>{PageName}</h3>
-          <p>Making hiring Process better and Smoother.</p>
-        </div>
-        <div className="footer-section">
-          <ul>
-            <li>
-              <h4>Home</h4>
-            </li>
-            <li>
-              <h4>Features</h4>
-            </li>
-            <li>
-              <h4>Team</h4>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-section">
-          <ul>
-            <li>
-              <h4>Pricing</h4>
-            </li>
-            <li>
-              <h4>Contact Us</h4>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-section">
-          <label htmlFor="callback">Request a callback</label>
-          <input
-            type="text"
-            id="callback"
-            placeholder="Enter your phone number"
-          />
-          <button type="submit">Submit</button>
-        </div>
-      </div>
-      <div className="footer-bottom">
-        <p>Copyright © 2024 {PageName}</p>
-        <ul className="social-icons">
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-              className="bi bi-facebook"
-            >
-              <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"></path>
-            </svg>
-          </li>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-              className="bi bi-twitter"
-            >
-              <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15"></path>
-            </svg>
-          </li>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-              className="bi bi-instagram"
-            >
-              <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"></path>
-            </svg>
-          </li>
-          <li>
-            {/* YouTube SVG */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-              className="bi bi-youtube"
-            >
-              <path d="M8.051 1.999C4.738 1.999 1.999 4.738 1.999 8.051s2.739 6.052 6.052 6.052 6.052-2.739 6.052-6.052-2.739-6.052-6.052-6.052zM8 0c4.418 0 8 3.582 8 8s-3.582 8-8 8-8-3.582-8-8 3.582-8 8-8zm-1.708 10.666v-5.332l5.332 2.666-5.332 2.666z" />
-            </svg>
-          </li>
-        </ul>
-      </div>
-    </footer>
-  );
+const Footer = () => {
+    return (
+        <footer className="footer-section">
+            <div className="container">
+                <div className="footer-cta pt-5 pb-5">
+                    <div className="row">
+                        <div className="col-xl-4 col-md-4 mb-30">
+                            <div className="single-cta">
+                                <i className="fas fa-map-marker-alt"></i>
+                                <div className="cta-text">
+                                    <h4>Find us</h4>
+                                    <span>1010 Avenue, sw 54321, chandigarh</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-4 col-md-4 mb-30">
+                            <div className="single-cta">
+                                <i className="fas fa-phone"></i>
+                                <div className="cta-text">
+                                    <h4>Call us</h4>
+                                    <span>9876543210 0</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-4 col-md-4 mb-30">
+                            <div className="single-cta">
+                                <i className="far fa-envelope-open"></i>
+                                <div className="cta-text">
+                                    <h4>Mail us</h4>
+                                    <span>mail@info.com</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-content pt-5 pb-5">
+                    <div className="row">
+                        <div className="col-xl-4 col-lg-4 mb-50">
+                            <div className="footer-widget">
+                                <div className="footer-logo">
+                                    <a href="index.html"><img src="https://i.ibb.co/QDy827D/ak-logo.png" className="img-fluid" alt="logo" /></a>
+                                </div>
+                                <div className="footer-text">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                </div>
+                                <div className="footer-social-icon">
+                                    <span>Follow us</span>
+                                    <a href="#"><i className="fab fa-facebook-f facebook-bg"></i></a>
+                                    <a href="#"><i className="fab fa-twitter twitter-bg"></i></a>
+                                    <a href="#"><i className="fab fa-google-plus-g google-bg"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
+                            <div className="footer-widget">
+                                <div className="footer-widget-heading">
+                                    <h3>Useful Links</h3>
+                                </div>
+                                <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Services</a></li>
+                                    <li><a href="#">Portfolio</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                    <li><a href="#">About us</a></li>
+                                    <li><a href="#">Our Services</a></li>
+                                    <li><a href="#">Expert Team</a></li>
+                                    <li><a href="#">Contact us</a></li>
+                                    <li><a href="#">Latest News</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
+                            <div className="footer-widget">
+                                <div className="footer-widget-heading">
+                                    <h3>Subscribe</h3>
+                                </div>
+                                <div className="footer-text mb-25">
+                                    <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+                                </div>
+                                <div className="subscribe-form">
+                                    <form action="#">
+                                        <input type="text" placeholder="Email Address" />
+                                        <button><i className="fab fa-telegram-plane"></i></button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="copyright-area">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+                            <div className="copyright-text">
+                                <p>Copyright &copy; 2018, All Right Reserved <a href="https://codepen.io/anupkumar92/">Anup</a></p>
+                            </div>
+                        </div>
+                        <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                            <div className="footer-menu">
+                                <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">Terms</a></li>
+                                    <li><a href="#">Privacy</a></li>
+                                    <li><a href="#">Policy</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 }
+
+export default Footer;
