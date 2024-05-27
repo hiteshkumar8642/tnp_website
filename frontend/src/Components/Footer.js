472cdc71f3efe1a1b168e4c7245657d2a7e4ca73
Sayantan Bairagi <sayantan259@gmail.com>
2024-05-27 22:53:50 +0530

    Footer Updated

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,121 +1,131 @@
-import React from 'react';
-import '../Styles/Footer.css';
+import React from "react";
+import "../Styles/Footer.css";
 
-const Footer = () => {
-    return (
-        <footer className="footer-section">
-            <div className="container">
-                <div className="footer-cta pt-5 pb-5">
-                    <div className="row">
-                        <div className="col-xl-4 col-md-4 mb-30">
-                            <div className="single-cta">
-                                <i className="fas fa-map-marker-alt"></i>
-                                <div className="cta-text">
-                                    <h4>Find us</h4>
-                                    <span>1010 Avenue, sw 54321, chandigarh</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xl-4 col-md-4 mb-30">
-                            <div className="single-cta">
-                                <i className="fas fa-phone"></i>
-                                <div className="cta-text">
-                                    <h4>Call us</h4>
-                                    <span>9876543210 0</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xl-4 col-md-4 mb-30">
-                            <div className="single-cta">
-                                <i className="far fa-envelope-open"></i>
-                                <div className="cta-text">
-                                    <h4>Mail us</h4>
-                                    <span>mail@info.com</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+export default function Footer() {
+  let PageName = "Placement";
+  return (
+    <footer className="footer-section">
+      <div className="container">
+        <div className="footer-cta pt-5 pb-5">
+          <div className="row">
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="fas fa-map-marker-alt"></i>
+                <div className="cta-text">
+                  <h4>Find us</h4>
+                  <span>NIT Jamshedpur, Jamshedpur</span>
                 </div>
-                <div className="footer-content pt-5 pb-5">
-                    <div className="row">
-                        <div className="col-xl-4 col-lg-4 mb-50">
-                            <div className="footer-widget">
-                                <div className="footer-logo">
-                                    <a href="index.html"><img src="https://i.ibb.co/QDy827D/ak-logo.png" className="img-fluid" alt="logo" /></a>
-                                </div>
-                                <div className="footer-text">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                </div>
-                                <div className="footer-social-icon">
-                                    <span>Follow us</span>
-                                    <a href="#"><i className="fab fa-facebook-f facebook-bg"></i></a>
-                                    <a href="#"><i className="fab fa-twitter twitter-bg"></i></a>
-                                    <a href="#"><i className="fab fa-google-plus-g google-bg"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
-                            <div className="footer-widget">
-                                <div className="footer-widget-heading">
-                                    <h3>Useful Links</h3>
-                                </div>
-                                <ul>
-                                    <li><a href="#">Home</a></li>
-                                    <li><a href="#">About</a></li>
-                                    <li><a href="#">Services</a></li>
-                                    <li><a href="#">Portfolio</a></li>
-                                    <li><a href="#">Contact</a></li>
-                                    <li><a href="#">About us</a></li>
-                                    <li><a href="#">Our Services</a></li>
-                                    <li><a href="#">Expert Team</a></li>
-                                    <li><a href="#">Contact us</a></li>
-                                    <li><a href="#">Latest News</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
-                            <div className="footer-widget">
-                                <div className="footer-widget-heading">
-                                    <h3>Subscribe</h3>
-                                </div>
-                                <div className="footer-text mb-25">
-                                    <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
-                                </div>
-                                <div className="subscribe-form">
-                                    <form action="#">
-                                        <input type="text" placeholder="Email Address" />
-                                        <button><i className="fab fa-telegram-plane"></i></button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
-            <div className="copyright-area">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-xl-6 col-lg-6 text-center text-lg-left">
-                            <div className="copyright-text">
-                                <p>Copyright &copy; 2018, All Right Reserved <a href="https://codepen.io/anupkumar92/">Anup</a></p>
-                            </div>
-                        </div>
-                        <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
-                            <div className="footer-menu">
-                                <ul>
-                                    <li><a href="#">Home</a></li>
-                                    <li><a href="#">Terms</a></li>
-                                    <li><a href="#">Privacy</a></li>
-                                    <li><a href="#">Policy</a></li>
-                                    <li><a href="#">Contact</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="fas fa-phone"></i>
+                <div className="cta-text">
+                  <h4>Call us</h4>
+                  <span>1234567890</span>
                 </div>
+              </div>
             </div>
-        </footer>
-    );
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="far fa-envelope-open"></i>
+                <div className="cta-text">
+                  <h4>Mail us</h4>
+                  <span>mail@info.com</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer-content pt-5 pb-5">
+          <div className="row">
+            <div className="col-xl-4 col-lg-4 mb-50">
+              <div className="footer-widget">
+                <div className="footer-logo">
+                  <h4>{PageName}</h4>
+                </div>
+                <div className="footer-text">
+                  <p>Making Placement process easier, better and smoother</p>
+                </div>
+                <div className="footer-social-icon">
+                  <span>Follow us</span>
+                  <a href="https://www.google.co.in/">
+                    <i className="fab fa-facebook-f facebook-bg"></i>
+                  </a>
+                  <a href="https://www.google.co.in/">
+                    <i className="fab fa-twitter twitter-bg"></i>
+                  </a>
+                  <a href="https://www.google.co.in/">
+                    <i className="fab fa-google-plus-g google-bg"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
+              <div className="footer-widget">
+                <div className="footer-widget-heading">
+                  <h3>Useful Links</h3>
+                </div>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Services</li>
+                  <li>Portfolio</li>
+                  <li>Contact</li>
+                  <li>About us</li>
+                  <li>Our Services</li>
+                  <li>Expert Team</li>
+                  <li>Contact us</li>
+                  <li>Latest News</li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
+              <div className="footer-widget">
+                <div className="footer-widget-heading">
+                  <h3>Subscribe</h3>
+                </div>
+                <div className="footer-text mb-25">
+                  <p>
+                    Don’t miss to subscribe to our new feeds, kindly fill the
+                    form below.
+                  </p>
+                </div>
+                <div className="subscribe-form">
+                  <form action="#">
+                    <input type="text" placeholder="Email Address" />
+                    <button>
+                      <i className="fab fa-telegram-plane"></i>
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="copyright-area">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+              <div className="copyright-text">
+                <p>Copyright &copy; 2024, All Right Reserved {PageName}</p>
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+              <div className="footer-menu">
+                <ul>
+                  <li>Home</li>
+                  <li>Terms</li>
+                  <li>Privacy</li>
+                  <li>Policy</li>
+                  <li>Contact</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
-
-export default Footer;
