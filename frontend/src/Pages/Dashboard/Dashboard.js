861da3b1e3f5edad1efd513c958aff9a0a699c47
Sayantan Bairagi <sayantan259@gmail.com>
2024-08-01 21:33:47 +0530

    Sidebar is moved out of DashBoard Content

--- frontend/src/Pages/Dashboard/Dashboard.js
+++ frontend/src/Pages/Dashboard/Dashboard.js
@@ -1,12 +1,16 @@
 import DashboardContent from "../../Components/DashboardContent/DashboardContent";
 import DashboardHeader from "../../Components/DashboardHeader/DashboardHeader";
+import Sidebar from "../../Components/Sidebar/Sidebar";
 import "./Dashboard.css";
 
 function Dashboard() {
   return (
     <div className="app-container">
       <DashboardHeader />
-      <DashboardContent />
+      <div className="app-content">
+        <Sidebar />
+        <DashboardContent />
+      </div>
     </div>
   );
 }
