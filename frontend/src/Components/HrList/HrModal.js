04972504bfa8d97ff89a4f1703fead2626d21637
Sayantan Bairagi <sayantan259@gmail.com>
2024-08-06 13:26:24 +0530

    Company information showing

--- frontend/src/Components/HrList/HrModal.js
+++ frontend/src/Components/HrList/HrModal.js
@@ -5,9 +5,6 @@ import "./HrModal.css";
 import { FaLinkedin } from "react-icons/fa";
 
 const HrModal = ({ hr, onClose, handleStatusChange }) => {
-  const [lastContactDate, setLastContactDate] = useState(
-    new Date(hr.last_date_of_contact)
-  );
   const [nextContactDate, setNextContactDate] = useState(
     new Date(hr.next_date_of_contact)
   );
@@ -40,23 +37,28 @@ const HrModal = ({ hr, onClose, handleStatusChange }) => {
         </div>
         <div className="hrlist-modal-body">
           <div className="grid-item grid-item-0-0">
-            <p>Company: {hr.company_id.name}</p>
-            <p>Gender: {hr.gender}</p>
-            <div>
+            <div className="info-row">
+              <label>Gender:</label>
+              <span className="data">{hr.gender}</span>
+            </div>
+            <div className="info-row">
               <label>Last Date of Contact:</label>
-              <DatePicker
-                selected={lastContactDate}
-                onChange={(date) => setLastContactDate(date)}
-              />
+              <div className="data">{hr.last_date_of_contact}</div>
             </div>
-            <div>
+            <div className="info-row">
               <label>Next Date of Contact:</label>
-              <DatePicker
-                selected={nextContactDate}
-                onChange={(date) => setNextContactDate(date)}
-              />
+              <div className="data">
+                <DatePicker
+                  selected={nextContactDate}
+                  onChange={(date) => setNextContactDate(date)}
+                  dateFormat="yyyy-MM-dd"
+                />
+              </div>
             </div>
-            <p>Status: {hr.status}</p>
+            <div className="info-row">
+              <label>Status:</label>
+              <span className="data">{hr.status}</span>
+            </div>
             <button onClick={() => handleStatusChange(hr.id, "Assigned")}>
               Assign me
             </button>
@@ -78,6 +80,31 @@ const HrModal = ({ hr, onClose, handleStatusChange }) => {
                 ))}
               </tbody>
             </table>
+          </div>
+          <div className="grid-item grid-item-1-0">
+            <h3>Company Information</h3>
+            <p>Company: {hr.company_id.name}</p>
+            <p>General CTC: {hr.company_id.general_ctc}</p>
+            <p>College CTC: {hr.company_id.college_ctc}</p>
+            <p>
+              Time of Visit:{" "}
+              {new Date(hr.company_id.time_of_visit).toLocaleDateString()}
+            </p>
+            <h4>Allowed Courses:</h4>
+            <ul>
+              {hr.company_id.allowed_courses.map((course, index) => (
+                <li key={index}>
+                  {course.degree} in {course.specialization} (
+                  {course.course_duration} years)
+                </li>
+              ))}
+            </ul>
+            <h4>Point of Contact:</h4>
+            <p>College: {hr.company_id.poc.college.name}</p>
+            <p>
+              Course: {hr.company_id.poc.course.degree} in{" "}
+              {hr.company_id.poc.course.specialization}
+            </p>
           </div>
           <div className="grid-item grid-item-1-1">
             <div>
