b72105b39c03c191c4a10ded23ff4bed35bdf403
Ritik Bansal <bansalritik108@gmail.com>
2024-08-10 16:03:08 +0530

    Created ResetPasswordToken

--- backend/userDetails/models.py
+++ backend/userDetails/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.mail import EmailMultiAlternatives
+from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django.urls import reverse
+from django.utils.html import strip_tags
+from django_rest_passwordreset.signals import reset_password_token_created
 
-# Create your models here.
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(reset_password_token,*args, **kwargs):
+    sitelink = "http://localhost:8000/"
+    token = "?token={}".format(reset_password_token.key)
+    full_link = str(sitelink) + str("password-reset") + str(token)
+
+    print(full_link)
