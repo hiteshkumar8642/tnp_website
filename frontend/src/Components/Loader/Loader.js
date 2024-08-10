aa7bef6eea7b65ce1a7b263cc2a7cea72c4444d7
Gaurav12341234 <gauravkcity16@gmail.com>
2024-08-10 22:59:58 +0530

    Responsive of landining  page, header etc..

--- frontend/src/Components/Loader/Loader.js
+++ frontend/src/Components/Loader/Loader.js
@@ -9,7 +9,7 @@ const Loader = () => {
 
   return (
     <div className="loader-overlay">
-      <div class="loader">
+      <div className="loader">
         <svg viewBox="0 0 80 80">
             <circle id="test" cx="40" cy="40" r="32"></circle>
         </svg>
--- frontend/src/Pages/LandingPage/LandingPage.js
+++ frontend/src/Pages/LandingPage/LandingPage.js
@@ -29,25 +29,30 @@ export default function LandingPage() {
 function LandingPageContent() {
   let PageName = "Placement";
   return (
-    <div className="content container flex items-center justify-between gap-5 min-h-[calc(100vh-80px)]">
-      <div className="info max-w-[50%] flex-1">
-        <h1 className="text-[#5d5d5d] text-5xl font-semibold pb-4">From Manual to Marvelous: Elevate Your Placements!</h1>
-        <p className="m-0 leading-loose text-sm text-[#5d5d5d]">
+    <div className="content container flex flex-col lg:flex-row items-center justify-between gap-5 min-h-[calc(100vh-80px)]">
+      <div className="info w-full lg:max-w-[50%] flex-1 text-center lg:text-left">
+        <h1 className="text-[#5d5d5d] text-3xl md:text-4xl lg:text-5xl font-semibold pb-4">
+          From Manual to Marvelous: Elevate Your Placements!
+        </h1>
+        <p className="m-0 leading-loose text-sm md:text-base text-[#5d5d5d]">
           Welcome to {PageName} – revolutionizing campus placements with our streamlined, automated platform. Bid farewell to manual hassles, and embrace innovation for student success. Elevate your campus dynamics with {PageName} – where smart placements meet smarter careers!
         </p>
         <Link to="/collegeRegistration">
-          <button className="primary-button bg-[#6c63ff] text-white py-2 px-9 mt-7 hover:bg-[#5752d8] transition-colors rounded-full">
+          <button className="primary-button bg-[#6c63ff] text-white py-2 px-6 md:px-9 mt-5 md:mt-7 hover:bg-[#5752d8] transition-colors rounded-full">
             Register your College
           </button>
         </Link>
       </div>
-      <div className="image max-w-[32%] flex-1">
+      <div className="image w-full lg:max-w-[32%] flex-1 mb-5 lg:mb-0">
         <img
           src="https://i.postimg.cc/65QxYYzh/001234.png"
           alt="Placement Illustration"
-          className="max-w-full block m-auto"
+          className="max-w-[80%] sm:max-w-[60%] lg:max-w-full block m-auto"
         />
       </div>
     </div>
   );
 }
+
+
+
