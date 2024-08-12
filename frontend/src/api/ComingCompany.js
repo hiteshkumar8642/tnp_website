a256e300eb17e503d168a1f7e5a5f57af161b4be
Sayantan Bairagi <sayantan259@gmail.com>
2024-08-12 12:40:50 +0530

    Share HR Contact and SharedHRLIST FIxed

--- frontend/src/api/ComingCompany.js
+++ frontend/src/api/ComingCompany.js
@@ -3,7 +3,6 @@ import apiClient from "../services/api";
 export async function fetchComingCompanyDetails() {
   try {
     const response = await apiClient.get("dashboard/api/application/");
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error("Failed to fetch company details", error);
